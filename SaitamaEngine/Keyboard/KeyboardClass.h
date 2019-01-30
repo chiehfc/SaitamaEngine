4a69b25a9f58148fc94ceef961026db26b553f46
Jeff Chen <chiehfc@uci.edu>
2019-01-29 17:29:08 -0800

    Keyboard class

--- SaitamaEngine/Keyboard/KeyboardClass.h
+++ SaitamaEngine/Keyboard/KeyboardClass.h
@@ -1,8 +1,20 @@
 #pragma once
+#include "KeyboardEvent.h"
+#include <queue>
+
 class KeyboardClass
 {
 public:
   KeyboardClass();
   ~KeyboardClass();
+
+
+
+private:
+  bool m_autoRepeatKeys = false;
+  bool m_autoRepeatChars = false;
+  bool m_keyStates[256];
+  std::queue<KeyboardEvent> m_keyBuffer;
+  std::queue<unsigned char> m_charBuffer;
 };
 
