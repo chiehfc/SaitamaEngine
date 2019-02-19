7eed7c8ab8f3f61de6f1d3649863a97fde6a1ebe
Jeff Chen <chiehfc@uci.edu>
2019-02-19 15:17:22 -0800

    Keyboard class

--- SaitamaEngine/Keyboard/KeyboardClass.cpp
+++ SaitamaEngine/Keyboard/KeyboardClass.cpp
@@ -4,9 +4,103 @@
 
 KeyboardClass::KeyboardClass()
 {
+  for (int i = 0; i < 256; i++)
+  {
+    this->m_keyStates[i] = false;
+  }
 }
 
 
 KeyboardClass::~KeyboardClass()
 {
+}
+
+bool KeyboardClass::KeyIsPressed(const unsigned char keycode)
+{
+  return this->m_keyStates[keycode];
+}
+
+bool KeyboardClass::KeyBufferIsEmpty()
+{
+  return this->m_keyBuffer.empty();
+}
+
+bool KeyboardClass::CharBufferIsEmpty()
+{
+  return this->m_charBuffer.empty();
+}
+
+KeyboardEvent KeyboardClass::ReadKey()
+{
+  if (this->m_keyBuffer.empty())
+  {
+    return KeyboardEvent();
+  }
+  else
+  {
+    KeyboardEvent e = this->m_keyBuffer.front();
+    this->m_keyBuffer.pop();
+    return e;
+  }
+}
+
+unsigned char KeyboardClass::ReadChar()
+{
+  if (this->m_charBuffer.empty())
+  {
+    return 0u;
+  }
+  else
+  {
+    unsigned char c = this->m_charBuffer.front();
+    this->m_charBuffer.pop();
+    return c;
+  }
+}
+
+void KeyboardClass::OnKeyPressed(const unsigned char key)
+{
+  this->m_keyStates[key] = true;
+  this->m_keyBuffer.push(KeyboardEvent(KeyboardEvent::EventType::Press, key));
+}
+
+void KeyboardClass::OnKeyReleased(const unsigned char key)
+{
+  this->m_keyStates[key] = false;
+  this->m_keyBuffer.push(KeyboardEvent(KeyboardEvent::EventType::Release, key));
+}
+
+void KeyboardClass::OnChar(const unsigned char key)
+{
+  this->m_charBuffer.push(key);
+}
+
+void KeyboardClass::EnableAutoRepeatKeys()
+{
+  this->m_autoRepeatKeys = true;
+}
+
+void KeyboardClass::DisableAutoRepeatKeys()
+{
+  this->m_autoRepeatKeys = false;
+}
+
+void KeyboardClass::EnableAutoRepeatChars()
+{
+  this->m_autoRepeatChars = true;
+}
+
+void KeyboardClass::DisableAutoRepeatChars()
+{
+  this->m_autoRepeatChars = false;
+}
+
+bool KeyboardClass::IsKeysAutoRepeat()
+{
+  return this->m_autoRepeatKeys;
+}
+
+bool KeyboardClass::IsCharsAutoRepeat()
+{
+  return this->m_autoRepeatChars;
 }
--- SaitamaEngine/Keyboard/KeyboardClass.h
+++ SaitamaEngine/Keyboard/KeyboardClass.h
@@ -8,7 +8,20 @@ public:
   KeyboardClass();
   ~KeyboardClass();
 
-
+  bool KeyIsPressed(const unsigned char keycode);
+  bool KeyBufferIsEmpty();
+  bool CharBufferIsEmpty();
+  KeyboardEvent ReadKey();
+  unsigned char ReadChar();
+  void OnKeyPressed(const unsigned char key);
+  void OnKeyReleased(const unsigned char key);
+  void OnChar(const unsigned char key);
+  void EnableAutoRepeatKeys();
+  void DisableAutoRepeatKeys();
+  void EnableAutoRepeatChars();
+  void DisableAutoRepeatChars();
+  bool IsKeysAutoRepeat();
+  bool IsCharsAutoRepeat();
 
 private:
   bool m_autoRepeatKeys = false;
